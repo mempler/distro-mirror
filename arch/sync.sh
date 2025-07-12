a8b3876fc2c0cdd6094d7f69c26f3b92dd65e79a
LunaStev <96914208+LunaStev@users.noreply.github.com>
2025-07-12 11:27:28 +0900

    change arch mirror url (#1)
    change arch mirror url

--- arch/sync.sh
+++ arch/sync.sh
@@ -5,7 +5,7 @@ set -e
 echo "[$(date)] sync.sh started" >> /var/log/sync.log
 
 MIRROR_DIR="/var/www/html/arch"
-REMOTE="rsync://mirrors.kernel.org/archlinux/"
+REMOTE="rsync://mirrors.edge.kernel.org/archlinux/"
 
 mkdir -p "$MIRROR_DIR"
 rsync -rtlH --delete-after --delay-updates --safe-links --progress "$REMOTE" "$MIRROR_DIR" >> /var/log/sync.log 2>&1
